5d367e5f8a9fb660d2000d2baa7df83b3f1143a8
Luvdduk <66945720+Luvdduk@users.noreply.github.com>
2020-09-13 18:20:23 +0900

    lcd 제어 추가

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import serial
 from PMS7003 import PMS7003
 import RPi.GPIO as GPIO
 
+import lcd_i2c as lcd
+
 
 
 power_state=0
+
+powersw = Button(26)
 
 #먼지센서 오브젝트
 dustlib = PMS7003()
@@ -19,10 +23,12 @@ SERIAL_PORT = '/dev/ttyUSB0'
 
 # led r:16 g:20 b:21
 # 릴레이: 19
-# fan = OutputDevice(19)
-fan = LED(19)
+fan = OutputDevice(19)
+# fan = LED(19)
 
 led = RGBLED(16, 20, 21)
+
+lcd.lcd_init()
 
 # led_r = LED(16)
 # led_g = LED(20)
@@ -37,7 +43,6 @@ led = RGBLED(16, 20, 21)
 def power():
     global power_state
     # 전원 스위치 gpio26
-    powersw = Button(26)
     powersw.when_pressed = powerctrl
     pause()
 # 꺼짐: 0, 켜짐: 1, 자동: 2
@@ -46,32 +51,36 @@ def powerctrl():
     if power_state == 0:
         power_state = 1
         print("전원켬")
+        # fan.on()
         return
     if power_state == 1:
         power_state = 2
         print("자동모드로 변경")
+        # if pm25 >=30 :
+        #     fan.on()
+        # else:
+        #     fan.off()
+        
         return
     if power_state == 2:
         power_state = 0
         print("전원끔")
+        # fan.off()
         return
 
 
-def fanCtrl():
-    global power_state
+# def fanCtrl():
+#     global power_state
 
-    if power_state == 0:
-        print("팬꺼짐")
-        fan.off()
-    if power_state == 1:
-        print("팬켜짐")
-        fan.on()
-    if power_state == 2:
-        print("팬자동")
-        if pm25 >=30 :
-            fan.on()
-        else:
-            fan.off()
+#     if power_state == 0:
+#         print("팬꺼짐")
+        
+#     if power_state == 1:
+#         print("팬켜짐")
+        
+#     if power_state == 2:
+#         print("팬자동")
+        
 
 
 
@@ -102,6 +111,42 @@ def fanCtrl():
 #         else:
 #             print ("data read Err")
 #         time.sleep(1)
+
+def display_dust(duststate1, duststate2, duststate3):
+    # 좋음
+    if duststate3 <= 30 and (duststate1 and duststate2) <= 15 :
+        lcd.lcd_string("     GOOD      ", lcd.LCD_LINE_1)
+    # 보통
+    elif  duststate3 <= 80 or (duststate1 or duststate2) <= 35:
+        lcd.lcd_string("     NORMAL    ", lcd.LCD_LINE_1)
+    # 나쁨
+    elif duststate3 <= 150 or (duststate1 or duststate2) <= 75:
+        lcd.lcd_string("      BAD      ", lcd.LCD_LINE_1)
+    # 매우나쁨
+    elif duststate3 > 150 or (duststate1 or duststate2) <= 75:
+        lcd.lcd_string("    VERY BAD   ", lcd.LCD_LINE_1)
+    
+    # pm1.0 표시
+    lcd.lcd_string("pm1.0: %dug/m3 " %duststate1, lcd.LCD_LINE_2)
+    if powersw.is_pressed:
+        return
+    time.sleep(1)
+    # pm2.5 표시
+    lcd.lcd_string("pm2.5: %dug/m3 " %duststate2, lcd.LCD_LINE_2)
+    if powersw.is_pressed:
+        return
+    time.sleep(1)
+    # pm10 표시
+    lcd.lcd_string("pm10: %dug/m3  " %duststate3, lcd.LCD_LINE_2)
+    if powersw.is_pressed:
+        return
+    time.sleep(1)
+    
+
+    
+    
+
+
 
 # 메인루프
 def mainloop():
@@ -139,11 +184,21 @@ def mainloop():
         if power_state == 0:
             print("전원꺼짐")
             fan.off()
+            lcd.LCD_BACKLIGHT = 0x00
+            lcd.lcd_string("   Power Off   ", lcd.LCD_LINE_1)
         if power_state == 1:
             print("전원켜짐")
+            lcd.LCD_BACKLIGHT = 0x08
+            lcd.lcd_string("   Power On!   ", lcd.LCD_LINE_1)
             fan.on()
+            time.sleep(1)
+            display_dust(int(pm1), int(pm25), int(pm10))
         if power_state == 2:
             print("자동모드")
+            lcd.LCD_BACKLIGHT = 0x08
+            lcd.lcd_string("   Auto Mode   ", lcd.LCD_LINE_1)
+            time.sleep(1)
+            display_dust(int(pm1), int(pm25), int(pm10))
             if pm25 >=30 :
                 fan.on()
             else:
