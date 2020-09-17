62a5866789ff9e2d313cd5234071258a2b365c80
Luvdduk <66945720+Luvdduk@users.noreply.github.com>
2020-09-17 13:27:57 +0900

    설정파일 추가, 모터제어추가

--- main.py
+++ main.py
@@ -6,44 +6,60 @@ import time
 import serial
 from PMS7003 import PMS7003
 import RPi.GPIO as GPIO
-
 import lcd_i2c as lcd
+from configparser import ConfigParser
 
 
 
-ON = 1
-OFF = 0
+# 설정파일 로드
+conf = ConfigParser()
+conf.read('config.ini')
 
+
+# 전원
 power_state=0
-
 powersw = Button(26)
+# 팬 버튼 24핀
+fansw= Button(24)
 
 #먼지센서 오브젝트
 dustlib = PMS7003()
 
+# 시리얼
 Speed = 9600
 SERIAL_PORT = '/dev/ttyUSB0'
 
-# led r:16 g:20 b:21
-# 릴레이: 19
-
+# 팬 조절
 fan_pwm = PWMOutputDevice(12)
 fan_pin1 = DigitalOutputDevice(5)
 fan_pin2 = DigitalOutputDevice(6)
 ON = 1
 OFF = 0
-HIGH = 1.0
+# 팬속
+FULL = 1.0
 MID = 0.65
-LOW = 0.3
-# fan = LED(19)
+SLOW = 0.3
 
+fan_pwm.value = conf['FAN']['fan_speed']
+if conf['FAN']['fan_speed'] == FULL:
+    fan_state = "FULL"
+elif conf['FAN']['fan_speed'] == MID:
+    fan_state = "MID"
+elif conf['FAN']['fan_speed'] == SLOW:
+    fan_state = "SLOW"
+else:
+    print("설정파일 로드 오류")
+
+
+
+
+# led rgb순
 led = RGBLED(16, 20, 21)
-
 # led_r = LED(16)
 # led_g = LED(20)
 # led_b = LED(21)
 
-
+# lcd초기화
 lcd.lcd_init()
 
 
@@ -52,6 +68,7 @@ lcd.lcd_init()
 def power():
     global power_state
     powersw.when_pressed = powerctrl
+    fansw.when_pressed = fan_speed
     pause()
 # 꺼짐: 0, 켜짐: 1, 자동: 2
 def powerctrl():
@@ -62,7 +79,6 @@ def powerctrl():
         lcd.LCD_BACKLIGHT = 0x08
         lcd.lcd_string("   Power On    ", lcd.LCD_LINE_1)
         powersw.wait_for_press(timeout=1.5)
-        # fan.on()
         return
     if power_state == 1:
         power_state = 2
@@ -70,10 +86,6 @@ def powerctrl():
         lcd.LCD_BACKLIGHT = 0x08
         lcd.lcd_string("   Auto Mode   ", lcd.LCD_LINE_1)
         powersw.wait_for_press(timeout=1.5)
-        # if pm25 >=30 :
-        #     fan.on()
-        # else:
-        #     fan.off()
         return
     if power_state == 2:
         power_state = 0
@@ -81,40 +93,12 @@ def powerctrl():
         lcd.LCD_BACKLIGHT = 0x00
         lcd.lcd_string("   Power Off   ", lcd.LCD_LINE_1)
         lcd.lcd_string("", lcd.LCD_LINE_2)
-        # fan.off()
         return
 
         
 
 
 
-
-#메인루프에 추가
-
-# def update_Dust():
-#     while True:
-#         #serial setting
-#         ser = serial.Serial(SERIAL_PORT, Speed, timeout = 1)
-#         buffer = ser.read(1024)
-
-#         if(dustlib.protocol_chk(buffer)):
-#             data = dustlib.unpack_data(buffer)
-
-#             pm1 = data[dustlib.DUST_PM1_0_ATM]
-#             pm25 = data[dustlib.DUST_PM2_5_ATM]
-#             pm10 = data[dustlib.DUST_PM10_0_ATM]
-            
-#             print ("PMS 7003 dust data")
-#             # print ("PM 1.0 : %s" % (data[dustlib.DUST_PM1_0_ATM]))
-#             # print ("PM 2.5 : %s" % (data[dustlib.DUST_PM2_5_ATM]))
-#             # print ("PM 10.0 : %s" % (data[dustlib.DUST_PM10_0_ATM]))
-#             print ("PM 1.0 : %s" % (pm1))
-#             print ("PM 2.5 : %s" % (pm25))
-#             print ("PM 10.0 : %s" % (pm10))
-
-#         else:
-#             print ("data read Err")
-#         time.sleep(1)
 
 def display_dust(duststate1, duststate2, duststate3):
     # 좋음
@@ -152,22 +136,32 @@ def display_dust(duststate1, duststate2, duststate3):
 
 
 
-
-def fan_ctrl(state, speed):
-    fan_pwm.value = speed
-    if state == 1:
+def fan_power(state):
+    if state:
         fan_pin1.on()
         fan_pin2.off()
-    if state == 0:
+    else:
         fan_pin1.off()
-        fan_pin2.on()
+        fan_pin2.off()
     
+def fan_speed(speed):
+    
+    if fan_state == "FULL":
+        fan_pwm.value = FULL
+        return
+    if fan_state == "MID":
+        fan_pwm.value = MID 
+        return
+    if fan_state == "SLOW":
+        fan_pwm.value = SLOW
+        return
+
 
 
 
 
 # 메인루프
-def mainloop():
+def loop():
     global power_state
     global led
 
@@ -202,13 +196,14 @@ def mainloop():
         if power_state == 0:
             print("전원꺼짐")
             # fan.off()
-            fan_ctrl(ON, )
+            fan_power(OFF)
             lcd.LCD_BACKLIGHT = 0x00
             lcd.lcd_string("   Power Off   ", lcd.LCD_LINE_1)
             lcd.lcd_string("", lcd.LCD_LINE_2)
             led.off()
         if power_state == 1:
             print("전원켜짐")
+            fan_power(ON)
             # lcd.LCD_BACKLIGHT = 0x08
             # lcd.lcd_string("   Power On    ", lcd.LCD_LINE_1)
             # fan.on()
@@ -221,17 +216,18 @@ def mainloop():
             # lcd.lcd_string("   Auto Mode   ", lcd.LCD_LINE_1)
             # time.sleep(1)
             if pm25 >=30 :
-                # fan.on()
+                fan_power(ON)
             else:
-                # fan.off()
+                fan_power(OFF)
             display_dust(int(pm1), int(pm25), int(pm10))
-        powersw.wait_for_press(timeout=2)
+        # powersw.wait_for_press(timeout=2)
+        time.sleep(1)
 
 
 
 if __name__ == "__main__":
     # 쓰레딩
     Thread(target=power).start()
-    Thread(target=mainloop).start()
+    Thread(target=loop).start()
     # Thread(target=update_Dust).start()
     
